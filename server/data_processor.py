5f84b55c0b30a52b07175dcc39ea92cea897bbe6
Joanna Misterka <joannamisterka@gmail.com>
2020-04-19 19:02:42 +0200

    moving generate alarm 2 methods to the class

--- server/data_processor.py
+++ server/data_processor.py
@@ -3,7 +3,7 @@ import datetime as dt
 import statistics as stat
 import logging
 from logging.handlers import RotatingFileHandler
-import request_handler
+from request_handler import add_alarm_to_alarm_stack
 import time
 
 logger = logging.getLogger(__name__)
@@ -30,13 +30,18 @@ pm10_model_values = [10, 25, 10]
 pm25_model_values = [10, 25, 9]
 
 
-class DataProcessor:
+class DataProcessor(object):
+    """ TO DO: Customize alarms """
 
-    def __init__(self):
-        self.result = 0
-        self.generate_alarms_for_all_sensors()
+    def __init__(self, sensor_id, value):
+        self.sensor_id = sensor_id
+        self.value = float(value)
+        self.is_alarm = False
+        self.alarm_type = None
+        self.result["is_alarm"] = 1
 
     def generate_alarms_for_all_sensors(self):
+
         while True:
             self.generate_alarm_type2_wrapper(1)
             self.generate_alarm_type2_wrapper(3)
@@ -69,12 +74,10 @@ class DataProcessor:
                                       variation_temperature)
 
     def generate_alarm_type2(self, sensor_id, start_time, stop_time, model_value, variation):
-        self.result = {}
         db = Database()
         logger.info("SENSOR ID" + str(sensor_id))
         measurements = db.get_sensor_measurements_from_time_period(sensor_id, start_time, stop_time)
         measurements_values = []
-        average = 0
 
         for measurement in measurements:
             measurements_values.append(measurement[0])
@@ -83,17 +86,13 @@ class DataProcessor:
             average = stat.mean(measurements_values)
             logger.info("MEAN" + str(average))
             if (model_value - variation) <= average or (model_value + variation >= average):
-                request_handler.add_alarm_to_alarm_stack("ALARM_TYPE_2", sensor_id, stop_time)
-                self.result["is_alarm"] = 1
+                logger.info("ADDING TO STACK")
+                add_alarm_to_alarm_stack("ALARM_TYPE_2", sensor_id, stop_time)
+                logger.info("IS ALARM TRUE")
+                self.is_alarm = True
                 logger.info("**********Alarm!!**********")
-                logger.info("Sensor ID: {}".format(sensor_id))
         else:
             logger.info("SLEEP")
-            time.sleep(600)
-            self.result["is_alarm"] = 0
-
-        logger.info("MEAN" + str(average))
-<<<<<<< HEAD
 
     def get_current_limit_values():
         db = Database()
@@ -103,149 +102,6 @@ class DataProcessor:
             sensor_limit_max[sensor[0]] = sensor[3]
         logger.info("Sensor limit min: {}".format(sensor_limit_min))
         logger.info("Sensor limit max: {}".format(sensor_limit_max))
-
-    class DataProcessor(object):
-        """ TO DO: Customize alarms """
-
-        def __init__(self, sensor_id, value):
-            self.sensor_id = sensor_id
-            self.value = float(value)
-            self.is_alarm = False
-            self.alarm_type = None
-
-        def do_statistics(self, sensor_id, measurement_type_id):
-            pm_2_5_max_threshold = 1000
-            pm_2_5_min_threshold = 0
-            temp_max_threshold = 1000
-            temp_min_threshold = 0
-            humi_max_threshold = 1000
-            humi_min_threshold = 0
-            c0_max_threshold = 1000
-            c0_min_threshold = 0
-            vibr_max_threshold = 1000
-            vibr_min_threshold = 0
-
-            #       consider case when sensor is temporarly unavailable
-            current_time = dt.datetime.now()
-            last_hour = dt.datetime.now() - dt.timedelta(hours=1)
-            #       FORMAT: 2020-03-17 18:38:32.674248
-            #        result = database.get_measurement(self, sensor_id, measurement_type_id, start_time = last_hour, stop_time = current_time):
-            #        i expect result = [1, 2, 3, 4, 5, 4, 6, 4, 5]
-
-            #       PM2.5
-            if measurement_type_id == 1:
-                if result[-1] > pm_2_5_max_threshold:
-                    self.raise_alarm()
-
-            if measurement_type_id == 1:
-                if result[-1] < pm_2_5_min_threshold:
-                    self.raise_alarm()
-
-            #       Temprature
-            if measurement_type_id == 2:
-                if result[-1] > temp_max_threshold:
-                    self.raise_alarm()
-
-            if measurement_type_id == 2:
-                if result[-1] < temp_min_threshold:
-                    self.raise_alarm()
-
-            #       Humidity
-            if measurement_type_id == 3:
-                if result[-1] > humi_max_threshold:
-                    self.raise_alarm()
-
-            if measurement_type_id == 3:
-                if result[-1] < humi_min_threshold:
-                    self.raise_alarm()
-
-            #       Carbon monoxide
-            if measurement_type_id == 4:
-                if result[-1] > c0_max_threshold:
-                    self.raise_alarm()
-
-            if measurement_type_id == 4:
-                if result[-1] < c0_min_threshold:
-                    self.raise_alarm()
-
-            #       Vibration
-            if measurement_type_id == 5:
-                if result[-1] > vibr_max_threshold:
-                    self.raise_alarm()
-
-            if measurement_type_id == 5:
-                if result[-1] < vibr_min_threshold:
-                    self.raise_alarm()
-
-            if len(result) > 10:
-                #           standard deviation
-                st_dev = statistics.stdev(result[:-1])
-                upper_threshold = fmean(result[:-1]) + st_dev
-                lower_threshold = fmean(result[:-1]) - st_dev
-                if result[-1] > upper_threshold or result[-1] < lower_threshold:
-                    raise_alarm(self)
-                    # customize alarm for each metric
-
-        def check_if_measurement_exceed_limits(self):
-            if self.value < sensor_limit_min[self.sensor_id]:
-                self.raise_alarm("LOW")
-            elif self.value > sensor_limit_max[self.sensor_id]:
-                self.raise_alarm("HIGH")
-
-        def count_the_average(self):
-            pass
-
-        def count_the_variance(self):
-            pass
-
-        def raise_alarm(self, alarm_t):
-            self.is_alarm = True
-            if alarm_t == "LOW":
-                self.alarm_type = "LOW"
-            elif alarm_t == "HIGH":
-                self.alarm_type = "HIGH"
-
-        def parse_received_data(self, txt):
-            #        self.save_parsed_data_in_database()
-            #        do_statistics on newest set of data from database just after save
-            #        do_statistics(self, sensor_id, measurement_type_id)
-
-            pass
-
-        def save_parsed_data_in_database(self, data):
-            pass
-
-        def send_results_to_application(self, results):
-            pass
-=======
-        if (model_value - variation) <= average or (model_value + variation >= average):
-            request_handler.add_alarm_to_alarm_stack("ALARM_TYPE_2", sensor_id, stop_time)
-            logger.info("**********Alarm!!**********")
-            logger.info("Sensor ID: {}".format(sensor_id))
-    else:
-        logger.info("SLEEP")
-        
-
-   
-
-def get_current_limit_values():
-    db = Database()
-    sensors_list = db.get_sensors_limit_values()
-    for sensor in sensors_list:
-        sensor_limit_min[sensor[0]] = sensor[2]
-        sensor_limit_max[sensor[0]] = sensor[3]
-    logger.info("Sensor limit min: {}".format(sensor_limit_min))
-    logger.info("Sensor limit max: {}".format(sensor_limit_max))
-
-
-class DataProcessor(object):
-    """ TO DO: Customize alarms """
-
-    def __init__(self, sensor_id, value):
-        self.sensor_id = sensor_id
-        self.value = float(value)
-        self.is_alarm = False
-        self.alarm_type = None
 
     def do_statistics(self, sensor_id, measurement_type_id):
         pm_2_5_max_threshold = 1000
@@ -351,4 +207,3 @@ class DataProcessor(object):
 
     def send_results_to_application(self, results):
         pass
->>>>>>> e9f9d6fb4b457144690a5f6bfa9fa404f7790aeb
